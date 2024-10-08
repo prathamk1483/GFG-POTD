9db6951105612120f39a73a531dd3af7d30f514f
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2024-10-08 18:54:42 +0530

    Update Largest Pair Sum 8th Oct 2024.java

--- Largest Pair Sum 8th Oct 2024.java
+++ Largest Pair Sum 8th Oct 2024.java
@@ -2,10 +2,11 @@ class Solution {
     public static int pairsum(int[] arr) {
         int l = Integer.MIN_VALUE , sl = Integer.MIN_VALUE;
         for(int i : arr){
-            l = Math.max(i,l);
-        }
-        for(int i : arr){
-            if(i > sl && i < l){
+            if(i > l){
+                sl = l;
+                l = i;
+            }
+            else if(i > sl){
                 sl = Math.max(i,sl);
             }
         }
