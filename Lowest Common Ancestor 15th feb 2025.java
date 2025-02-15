e22c2b6ba1194a8e72bcf62ee411cee11efe99b3
Pratham Kubetkar <pratham.kubetkar21@pccoepune.org>
2025-02-15 09:14:40 +0530

    Create Lowest Common Ancestor 15th feb 2025.java

---
+++ Lowest Common Ancestor 15th feb 2025.java
@@ -0,0 +1,7 @@
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        if(root==null || root.data==n1.data || root.data==n2.data || (n1.data<root.data && root.data<n2.data) || (n2.data<root.data && root.data<n1.data)) return root;
+        if(root.data<n1.data && root.data<n2.data) return LCA(root.right,n1,n2);
+        return LCA(root.left,n1,n2);
+    }
+}
